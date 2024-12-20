060590c87cd4d031f687e321368f30218354ddfe
omar <oh1149725@gmail.com>
2024-12-20 19:00:24 +0200

    Add project files.

---
+++ C#Assignment5/Program.cs
@@ -0,0 +1,455 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace C_Assignment5
+{
+
+
+    #region Q1 enum
+    enum WeekDays
+    {
+
+        Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday
+    }
+
+    #endregion
+
+
+
+    #region Q2 and Q3 struct
+
+    struct Person
+    {
+
+
+        private int age;
+        private string Name;
+
+
+
+
+        public int _Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+
+        public string _Name
+        {
+            get { return Name; }
+            set { Name = value; }
+        }
+
+
+
+
+        public Person(int age, string name)
+        {
+            this.age = age;
+            this.Name = name;
+        }
+
+
+        public Person()
+        {
+
+            this.age = 0;
+            this.Name = "";
+
+
+        }
+
+
+
+
+        public static void GetOldest(Person p1 , Person p2 , Person p3)
+        {
+
+
+
+            if (p1._Age >= p2._Age && p1._Age>=p3._Age)
+            {
+
+               Console.WriteLine( p1.ToString());
+            }
+
+            else if(p2._Age >= p1._Age && p2._Age >= p3._Age)
+            {
+
+
+                Console.WriteLine(p2.ToString());
+            }
+
+            else
+            {
+
+                Console.WriteLine(p3.ToString());
+            }
+          
+
+
+        }
+
+
+
+
+
+        public override string ToString()
+        {
+
+
+            return ($" Name is : {this.Name} , Age : {this.age}");
+        }
+
+
+    }
+
+    #endregion
+
+
+
+    #region Q3 enum
+
+
+    enum Season
+    {
+        Spring, Summer, Autumn, winter
+    }
+
+    public enum Months
+    {
+
+        March = 1,
+        April = 2,
+        May = 3,
+        June = 4,
+        July = 5,
+        August = 6,
+        September = 7,
+        October = 8,
+        November = 9,
+        December = 10,
+        January = 11,
+        February = 12,
+    }
+
+    #endregion
+
+
+    #region Q4 enum
+
+    [Flags]
+    enum Permissions
+    {
+        None = 0,
+        Read = 1, Write = 2, Delete = 4, Execute = 8
+
+    }
+    #endregion
+
+
+
+    #region Q5 enum
+    enum Colors
+    {
+
+        Red, Blue, Green
+    }
+
+    #endregion
+
+
+    #region Q6 struct
+
+
+
+    struct Point
+    {
+
+
+       public double X { get; set; }
+       public double Y { get; set; }
+
+
+
+        public Point(double x, double y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+       public Point()
+        {
+
+            this.X = 0;
+            this.Y = 0;
+
+
+        }
+
+        public static double CalcualateDistance(Point p1, Point p2)
+        {
+
+
+            return (Math.Sqrt(Math.Pow((p1.X - p2.X), 2) + Math.Pow((p1.Y - p2.Y), 2)));
+        }
+
+    }
+
+
+
+    #endregion
+
+
+
+
+
+
+
+
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            #region Q1
+
+            //foreach (WeekDays day in Enum.GetValues(typeof(WeekDays))) // strings -> sunady
+            //{
+            //    Console.WriteLine(day);
+            //}
+
+            #endregion
+
+
+            //-------------------------------------
+
+
+            #region Q2
+
+
+            //Person []arr = new Person[3];
+
+            //arr[0] = new Person(20, "omar");
+            //arr[1] = new Person(19, "Ahmed");
+            //arr[2] = new Person(18, "Mohamed");
+
+
+
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    Console.WriteLine(arr[i].ToString());
+            //}
+
+            #endregion
+
+
+            //-----------------------------------
+
+
+            #region Q3
+
+
+            //Console.WriteLine("Enter Season Name :");
+
+            //string SeasonName = Console.ReadLine();
+
+
+            //if (Enum.TryParse(typeof(Season), SeasonName, true, out object Valid))
+            //{
+
+
+            //    Season S = (Season)Enum.Parse(typeof(Season),SeasonName,true);
+
+
+            //    if ((int)S == 0)
+            //    {
+
+            //        for (int i =1; i <= 3; ++i)
+            //        {
+            //            Console.Write((Months)i+" ");
+            //        }
+
+            //    }
+
+            //    else if ((int)S == 1)
+            //    {
+
+            //        for (int i =4; i <=6; ++i)
+            //        {
+            //            Console.Write((Months)i+" ");
+            //        }
+
+            //    }
+
+
+            //    else if ((int)S == 2)
+            //    {
+
+            //        for (int i =7; i <= 9; ++i)
+            //        {
+            //            Console.Write((Months)i+" ");
+            //        }
+
+            //    }
+
+
+            //    else
+            //    {
+            //        for (int i =10; i <= 12; ++i)
+            //        {
+            //            Console.Write((Months)i+" ");
+            //        }
+
+
+            //    }
+
+
+            #endregion
+
+
+
+            //----------------------------------------------------------
+            #region Q4
+
+
+            //Permissions P= Permissions.Read | Permissions.Write;
+
+
+            ////add Permissions
+            //P|= Permissions.Delete;
+
+
+            //// check If permissions Exist
+
+
+            //if ((P & Permissions.Execute) == Permissions.Execute)
+            //{
+            //    Console.WriteLine(" exists");
+            //}
+
+            //else
+            //{
+            //    Console.WriteLine(" does not exists");
+            //}
+
+
+            //// Remove permission
+
+            //P&=~ Permissions.Write;
+
+
+            #endregion
+
+
+            //-----------------------------------------------
+
+            #region Q5
+
+            //Console.WriteLine("Enter a Color :");
+
+            //string color=Console.ReadLine();
+
+
+
+
+
+            //if (Enum.TryParse(typeof(Colors),color , true , out object Valid))
+            //{
+
+            //    Console.WriteLine($" {color} is a Primary color");
+
+            //}
+
+            //else
+            //{
+            //    Console.WriteLine($" {color} is not a Primary color");
+
+            //}
+
+            //-------------------------------
+            #endregion
+
+
+            //-----------------------------------------------
+
+            #region Q6
+
+
+            //Point p1, p2;
+            //p1 = new Point();
+            //p2 = new Point();
+
+
+            //Console.WriteLine("Enter X for Point 1 :");
+            //p1.X=double.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Enter Y for Point 1 :");
+            //p1.Y=double.Parse(Console.ReadLine());
+
+
+
+            //Console.WriteLine("Enter X for Point 2 :");
+            //p2.X = double.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Enter Y for Point 2 :");
+            //p2.Y = double.Parse(Console.ReadLine());
+
+
+
+            //Console.WriteLine($" Distance is {Point.CalcualateDistance(p1, p2)}");
+
+
+
+            #endregion
+
+
+            //--------------------------
+
+
+            #region Q7
+
+            //Person p1=new Person(12,"omar");
+            //Person p2=new Person();
+            //Person p3=new Person();
+
+
+            //Console.WriteLine(" Enter Name for person 1");
+            //p1._Name = Console.ReadLine();
+
+            //Console.WriteLine("Enter Age of Person 1");
+            //p1._Age=int.Parse(Console.ReadLine());
+
+
+
+            //Console.WriteLine(" Enter Name for person 2");
+            //p2._Name = Console.ReadLine();
+
+
+            //Console.WriteLine("Enter Age of Person 2");
+            //p2._Age = int.Parse(Console.ReadLine());
+
+
+
+            //Console.WriteLine(" Enter Name for person 3");
+            //p3._Name = Console.ReadLine();
+
+
+
+            //Console.WriteLine("Enter Age of Person 3");
+            //p3._Age = int.Parse(Console.ReadLine());
+
+
+
+            //Person.GetOldest(p1, p2, p3);
+
+
+
+
+
+            #endregion
+        }
+    }
+    }
---
+++ Demo/Program.cs
@@ -0,0 +1,306 @@
+﻿
+using System.ComponentModel;
+using System.Xml.Linq;
+
+
+
+namespace Demo
+{
+
+    enum WeekDays
+    {
+
+        Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday
+
+
+
+    }
+
+
+    class Car
+    {
+
+        protected internal string name;
+        internal int model;
+        private protected int num;
+
+    
+    }
+
+    class student
+    {
+
+        int age;
+
+
+
+        public student() {
+
+            Car x = new Car();
+
+            Class1 z = new Class1();
+        }
+
+    }
+
+
+
+    class Account
+    {
+
+        private int ID;
+        private string name;
+        private string password;
+
+
+        // Automatic properties
+        public int _GetID { get { return ID; } set { ID = value; } }
+
+        public string getName { get { return name; } set { name = value; } }
+
+        public string GetPassword { get { return password; } set { password = value; } }
+
+
+        // Public getter, private setter
+        public string Name
+        {
+            get { return name; }
+            private set { name = value; }
+        }
+
+
+
+        public Account(int id , string name , string pass )
+        {
+
+            this.ID = id;
+            this.name = name;
+            this.password = pass;
+        }
+
+        public override string ToString()
+        {
+
+
+            return ($" name : {this.name} , ID : {this.ID}");
+        }
+
+    }
+
+
+    public interface ISYS
+    {
+
+
+
+
+        protected void fun2();
+    }
+
+        struct memeber
+        {
+
+
+            public int age;
+
+     
+
+            //protected int x; ->// not supported in stucts
+        }
+
+
+        [Flags]  // -> flag enum
+        enum colors
+        {
+
+
+            none = 0, red = 1, blue = 2, green = 4, yellow = 8  // powers of 2 beacuse bitwise calculations without overlap
+        }
+        class Perimeter
+        {
+
+            // declaring enum 
+            public enum shapes
+            {
+                circle,
+                square
+            }
+
+            public void peri(int val, shapes s1)
+            {
+
+                // checking for shape to be circle
+                if (s1 == 0)
+                {
+
+                    // Output the circumference
+                    Console.WriteLine("Circumference of the circle is " +
+                                                          2 * 3.14 * val);
+                }
+
+                else
+                {
+
+                    // else output the perimeter of the square
+                    Console.WriteLine("Perimeter of the square is " +
+                                                             4 * val);
+                }
+            }
+        }
+
+        public enum gender : byte
+        {
+
+            male, female
+        }
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+
+
+                #region Namespaces 
+
+                //use the using directive to bring a namespace into scope
+
+                // INside Namespace:
+                // 1- class , struct , enum , Intreface , event , Delegates
+
+                #endregion
+
+
+                #region Enums
+
+                // Enums is Value Type
+
+
+                // Cast Enums to int
+
+                WeekDays x = WeekDays.Sunday;
+
+                WeekDays z = (WeekDays)3; // int to enum
+
+
+                Console.WriteLine(x);
+
+                WeekDays n = (WeekDays)Enum.Parse(typeof(WeekDays), "monday", true); // -> converts string t enum type
+
+                //(WeekDays)Enum.Parse(typeof(WeekDays), "sunday",true) -> true -> case insensitve
+
+                Console.WriteLine(Enum.IsDefined(typeof(WeekDays), "Sunday"));
+
+                // -> Use Enum.IsDefined() to check if a value or name exists in the enum.
+
+
+                //  Enum.GetNames(typeof(WeekDays)) -> gets as strings
+
+                //  Enum.GetValues()  - > to retrieve an array of all enum members.
+
+                // Enum.Parse(typeof(WeekDays), "monday",false) -> gives error if string not found in enum
+
+                //Console.WriteLine(Enum.TryParse(typeof(Colors), "red", true ,out var result)); -> outputs true or false
+                // -> Check if the  input matches any value in the Colors enum
+
+
+
+                // flags enum used for ->File Permissions:
+                //Read, Write, Execute, etc.
+
+
+                //Use the bitwise OR operator (|) to combine flags
+
+                colors p = colors.blue | colors.green;
+
+                Console.WriteLine(p);
+
+                //se the bitwise AND operator (&) to check if a specific flag is set.
+
+                if ((p & colors.red) == colors.red)
+                {
+
+                    Console.WriteLine("yes");
+                }
+                else
+                {
+                    Console.WriteLine("no");
+                }
+
+            //Use the bitwise AND NOT operator (& ~) to remove a flag.
+            //--------------------------
+
+            #endregion
+
+
+
+            #region Access Modifiers
+
+
+            // Allowed Acces Modifiers - >Top-level classes, structs, enums, and interfaces can only use public and internal
+            // Internal -> full access to all modifiers
+
+
+            // In struct :
+            //allowed - > public, internal , protected
+            //Structs do not support inheritance, so protected, protected internal,
+            //and private protected are not valid for struct member
+
+
+            // The default access modifier of attributes and methods in class , strct is private 
+            // The default access modifier of Top level class , strct is internal
+
+            //protected internal -> Accessible within the same assembly or in derived classes outside the assembly.
+
+
+            //private protected :Accessible within the containing type and derived classes, but only
+            //if they are in the same assembly.
+
+
+            //What Interfaces Can Contain:
+            //Methods(with no implementation).
+            //Properties(with no implementation, just getters / setters).
+            //Events(declarations).
+            //Indexers(declarations).
+
+
+            //interface members can only be public, and cannot be declared with protected, private, or
+            //internal access modifiers.
+
+
+
+
+
+
+
+            #endregion
+
+
+
+            #region Encpsulation
+
+            /*Getter and Setter:
+            Properties consist of two key components:
+
+            Getter (get): Retrieves the value of the property.
+            Setter (set): Sets the value of the property
+                         
+             */
+
+
+            Account A1 = new Account(1, "omar", "@312");
+
+            A1.getName="ahmed";
+            Console.WriteLine(A1.getName);
+            Console.WriteLine(A1);
+
+
+
+
+            #endregion
+
+
+
+
+        }
+    }
+    }
+
